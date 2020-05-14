f99865801e46c7846b2038d497cf201e6414408f
Corne Siebeling <2078538@talnet.nl>
2020-05-14 12:50:33 +0200

    reken taak 1

---
+++ 4-Arithmetics/taak01/index.php
@@ -0,0 +1,17 @@
+<?php
+$getal1 = 5;
+$getal2 = 10;
+echo $getal1 / $getal2;
+
+$getal1 = 5;
+$getal2 = 10;
+echo $getal1 - $getal2;
+
+$getal1 = 5;
+$getal2 = 10;
+echo $getal1 + $getal2;
+
+$getal1 = 5;
+$getal2 = 10;
+echo $getal1 * $getal2;
+?>
