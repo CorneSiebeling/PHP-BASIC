ae3ef5f455a5faedfb137f3147a5f20bfee2d5e9
Corne Siebeling <2078538@talnet.nl>
2020-05-24 17:02:13 +0200

    taak 6 statements

---
+++ 5-Conditional_Statements/taak06/index.php
@@ -0,0 +1,39 @@
+<?php
+$var1 = 3;
+
+if($var1 < 10 && $var1 > 5) {
+    echo "het getal zit tussen de 10 en de 5!";
+} else {
+    echo "het getal is groter dan 10 of kleiner dan 5!";
+}
+
+
+$var2 = 4;
+
+if($var2 < 10 && $var2 > 5) {
+    echo "het getal zit tussen de 10 en de 5!";
+} else {
+    echo "het getal is groter dan 10 of kleiner dan 5!";
+}
+
+
+$var3 = 5;
+
+if($var3 < 10 && $var3 > 5) {
+    echo "het getal zit tussen de 10 en de 5!";
+} else {
+    echo "het getal is groter dan 10 of kleiner dan 5!";
+}
+
+$var4 = 2;
+
+if($var4 < 10 && $var4 > 5) {
+    echo "het getal zit tussen de 10 en de 5!";
+} else {
+    echo "het getal is groter dan 10 of kleiner dan 5!";
+}
+
+
+
+
+?>
