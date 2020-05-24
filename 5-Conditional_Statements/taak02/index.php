6e0ebc58c885d7492ac33e70fd02e95d6e30100a
Corne Siebeling <2078538@talnet.nl>
2020-05-24 16:51:33 +0200

    taak 2

---
+++ 5-Conditional_Statements/taak02/index.php
@@ -0,0 +1,16 @@
+<?php
+$variabele1 = 10;
+$variabele2 = 10;
+
+if($variabele1 == $variabele2) {
+    echo "de vergelijking is waar!";
+}
+
+
+$variabele3 = 10;
+$variabele4 = 12;
+
+if($variabele1 != $variabele2) {
+    echo "de vergelijking is onjuist!";
+}
+?>
