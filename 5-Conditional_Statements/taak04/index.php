8e63454a9bd812446a7a61a3d060dd2186a47be7
Corne Siebeling <2078538@talnet.nl>
2020-05-24 16:59:03 +0200

    taak 4 statements

---
+++ 5-Conditional_Statements/taak04/index.php
@@ -0,0 +1,91 @@
+<?php
+
+
+$score1 = 10;
+
+switch($score1) {
+    case 10:
+    echo "Wow wat een fantastische score!";
+    break;
+    case 9:
+    echo "Wow bijna alle punten nog 1 te gaan!";
+    break;
+    case 8:
+    echo "Goed bezig, bijna alle punten!";
+    break;
+    case 7:
+    echo "Ga zo door!";
+    break;
+    default:
+    echo "Nog even oefenen dan haal je vast alle punten!";
+}
+
+echo $score1;
+
+$score2 = 9;
+
+switch($score2) {
+    case 10:
+    echo "Wow wat een fantastische score!";
+    break;
+    case 9:
+    echo "Wow bijna alle punten nog 1 te gaan!";
+    break;
+    case 8:
+    echo "Goed bezig, bijna alle punten!";
+    break;
+    case 7:
+    echo "Ga zo door!";
+    break;
+    default:
+    echo "Nog even oefenen dan haal je vast alle punten!";
+}
+
+echo $score2;
+
+
+$score3 = 8;
+
+switch($score3) {
+    case 10:
+    echo "Wow wat een fantastische score!";
+    break;
+    case 9:
+    echo "Wow bijna alle punten nog 1 te gaan!";
+    break;
+    case 8:
+    echo "Goed bezig, bijna alle punten!";
+    break;
+    case 7:
+    echo "Ga zo door!";
+    break;
+    default:
+    echo "Nog even oefenen dan haal je vast alle punten!";
+}
+
+
+echo $score3;
+
+$score4 = 7;
+
+switch($score4) {
+    case 10:
+    echo "Wow wat een fantastische score!";
+    break;
+    case 9:
+    echo "Wow bijna alle punten nog 1 te gaan!";
+    break;
+    case 8:
+    echo "Goed bezig, bijna alle punten!";
+    break;
+    case 7:
+    echo "Ga zo door!";
+    break;
+    default:
+    echo "Nog even oefenen dan haal je vast alle punten!";
+}
+
+echo $score4;
+
+
+?>
