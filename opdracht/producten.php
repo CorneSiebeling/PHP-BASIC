c75b36b73397d17e210b6873aa85a54eaf448c74
Corne Siebeling <2078538@talnet.nl>
2020-06-30 15:12:54 +0200

    eindopdracht phph basic

--- opdracht/producten.php
+++ opdracht/producten.php
@@ -4,10 +4,17 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Title</title>
-  </head>
-    <form action="producten.php" method="get"><!-- dit laten staan! -->
+    <title>Eindoprdracht</title>
 
+    <?php 
+
+$producten = array("bal","rugbybal","tennisbal","hockeybal","hockeypuck","basketbal","voetbal","handbal","softbal","honkbal");
+$prijs = array(0.20,0.12,0.30,0.40,0.20,0.15,1.20,0.80,0.60,0.20);
+
+    include "includes/functions.php"?>
+
+  </head>
+  <form action="producten.php" method="get"><!-- dit laten staan! -->
     <p>Hieronder een lijst met producten die je bij ons kunt bestellen en de prijs per stuk:</p>
         <table>
             <tr>
@@ -15,16 +22,22 @@
                 <th>product</th>
                 <th>prijs per stuk</th>
             </tr>
-            <tr>
-                <td>0</td>
-                <td>appels</td>
-                <td>0,25</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>peren</td>
-                <td>0,30</td>
-            </tr>
+            <?php 
+
+
+echo "<tr>" . "<td>" . "0" . $producten[0] . "<td>" . $prijs[0];
+echo "<tr>" . "<td>" . "1" . $producten[1] . "<td>" . $prijs[1];
+echo "<tr>" . "<td>" . "2" . $producten[2] . "<td>" . $prijs[2];
+echo "<tr>" . "<td>" . "3" . $producten[3] . "<td>" . $prijs[3];
+echo "<tr>" . "<td>" . "4" . $producten[4] . "<td>" . $prijs[4];
+echo "<tr>" . "<td>" . "5" . $producten[5] . "<td>" . $prijs[5];
+echo "<tr>" . "<td>" . "6" . $producten[6] . "<td>" . $prijs[6];
+echo "<tr>" . "<td>" . "7" . $producten[7] . "<td>" . $prijs[7];
+echo "<tr>" . "<td>" . "8" . $producten[8] . "<td>" . $prijs[8];
+echo "<tr>" . "<td>" . "9" . $producten[9] . "<td>" . $prijs[9];
+
+
+?>  
         </table>
 
     <p>Vul hieronder het productnummer in van het product wat je wilt bestellen en de hoeveelheid.</p> 
@@ -33,9 +46,10 @@
     <p>Vul je leeftijd in voor korting</p>
     <p><label>leeftijd: </label><input type="text" name="leeftijd"></p><!-- dit laten staan! -->
     <p><button type="submit" name="submit">Bereken totaal</button></p><!-- dit laten staan! -->
-    <p>voor <?php getAmount(); ?> x het product: <?php getProductNr(); ?> betaal je:</p>
-    <p>je bent <?php getAge(); ?> je korting is: </p>
-
+    <p>voor <?php getAmount(); ?> x het product: <?php getProductNr(); ?> betaal je:</p> <?php getTotaal() ?>
+    <p>je bent <?php getAge(); ?> je korting is: </p> <?php getKorting(); ?>
+    <div>
+    </div>
     </form><!-- dit laten staan! -->
   </body>
 </html>
